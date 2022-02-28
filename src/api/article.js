ff647dd155a56f7549ad58bd1d9a6d652d8804b3
happy <997401767@qq.com>
2022-02-28 22:21:38 +0800

    新增文章管理需求

--- src/api/article.js
+++ src/api/article.js
@@ -1,41 +1,58 @@
 import request from '@/utils/request'
 
+// 获取文章列表
 export function fetchList(query) {
   return request({
-    url: '/vue-element-admin/article/list',
-    method: 'get',
+    url: `/vue-element-admin/article/list`,
+    method: `get`,
     params: query
   })
 }
 
+// 获取特定文章
 export function fetchArticle(id) {
   return request({
-    url: '/vue-element-admin/article/detail',
-    method: 'get',
+    url: `/vue-element-admin/article/detail`,
+    method: `get`,
     params: { id }
   })
 }
 
-export function fetchPv(pv) {
-  return request({
-    url: '/vue-element-admin/article/pv',
-    method: 'get',
-    params: { pv }
-  })
-}
-
+// 新建文章
 export function createArticle(data) {
   return request({
-    url: '/vue-element-admin/article/create',
-    method: 'post',
+    url: `/vue-element-admin/article/create`,
+    method: `post`,
     data
   })
 }
 
+
+
+// 更新文章
 export function updateArticle(data) {
   return request({
-    url: '/vue-element-admin/article/update',
-    method: 'post',
+    url: `/vue-element-admin/article/update`,
+    method: `post`,
     data
+  })
+}
+
+// 发布或草稿文章
+export function publishOrDraftArticle(data) {
+  return request({
+    url: `/vue-element-admin/article/update`,
+    method: `post`,
+    data
+  })
+}
+
+
+
+//删除文章
+export function deleteArticle(id) {
+  return request({
+    url: `/vue-element-admin/article/update/${id}`,
+    method: `delete`,
   })
 }
--- src/constant/type.js
+++ src/constant/type.js
@@ -1,23 +1,33 @@
-const ARTICLE_CATEGORY = [
-    "前端",
-    "后端",
-    "算法",
-    "人工智能",
-    "产品",
-    "设计",
-    "Android",
-    "IOS",
-    "运维",
-    "测试"
+export const ARTICLE_CATEGORY = [
+    { key: 0, display_name: '前端' },
+    { key: 1, display_name: '后端' },
+    { key: 2, display_name: '算法' },
+    { key: 3, display_name: '人工智能' },
+    { key: 4, display_name: '产品' },
+    { key: 5, display_name: '设计' },
+    { key: 6, display_name: 'Android' },
+    { key: 7, display_name: 'IOS' },
+    { key: 8, display_name: '运维' },
+    { key: 9, display_name: '测试' }
 ]
 
-const ARTICLE_TAG = [
-    "HTML",
-    "CSS",
-    "Javascript",
-    "Vue",
-    "React",
-    "Angular",
-    "Node.js",
-    "Webpack",
+export const ARTICLE_TAG = [
+    { key: 0, display_name: 'HTML' },
+    { key: 1, display_name: 'CSS' },
+    { key: 2, display_name: 'Javascript' },
+    { key: 3, display_name: 'Vue' },
+    { key: 4, display_name: 'React' },
+    { key: 5, display_name: 'Angular' },
+    { key: 6, display_name: 'Node.js' },
+    { key: 7, display_name: 'Webpack' }
 ]
+
+export const ARTICLE_CATEGORY_KV = KV(ARTICLE_CATEGORY)
+export const ARTICLE_TAG_KV = KV(ARTICLE_TAG)
+
+function KV(obj) {
+    return obj.reduce((acc, cur) => {
+        acc[cur.key] = cur.display_name
+        return acc
+    }, {})
+}
